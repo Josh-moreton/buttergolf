7c84d161f1bf540912e9491441388dd1057d469c
Josh <144067649+Josh-moreton@users.noreply.github.com>
2025-10-30 15:15:06 +0000

    linting

--- packages/config/src/tamagui.config.ts
+++ packages/config/src/tamagui.config.ts
@@ -2,17 +2,17 @@ import { defaultConfig } from '@tamagui/config/v4'
 import { createTamagui } from 'tamagui'
 
 export const config = createTamagui({
-  ...defaultConfig,
-  settings: {
-    ...defaultConfig.settings,
-    onlyAllowShorthands: false,
-  },
+    ...defaultConfig,
+    settings: {
+        ...defaultConfig.settings,
+        onlyAllowShorthands: false,
+    },
 })
 
 export type AppConfig = typeof config
 
 declare module 'tamagui' {
-  interface TamaguiCustomConfig extends AppConfig {}
+    interface TamaguiCustomConfig extends AppConfig { }
 }
 
 export default config
