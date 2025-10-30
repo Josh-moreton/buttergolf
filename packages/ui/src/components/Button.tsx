0db8118962b59dc0032fa139bdf7728e0b9e5381
Josh <144067649+Josh-moreton@users.noreply.github.com>
2025-10-30 10:47:46 +0000

    fix type error in button

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,22 +1,3 @@
-// Simple re-export from Tamagui core
-import { styled, Stack } from 'tamagui'
-
-export const Button = styled(Stack, {
-  name: 'Button',
-  tag: 'button',
-  backgroundColor: '$background',
-  paddingHorizontal: '$4',
-  paddingVertical: '$2',
-  borderRadius: '$4',
-  cursor: 'pointer',
-  
-  pressStyle: {
-    opacity: 0.8,
-  },
-  
-  hoverStyle: {
-    backgroundColor: '$backgroundHover',
-  },
-})
-
-export type ButtonProps = React.ComponentProps<typeof Button>
+// Re-export Button from Tamagui
+export { Button } from '@tamagui/button'
+export type { ButtonProps } from '@tamagui/button'
