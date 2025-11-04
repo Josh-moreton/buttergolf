617d371f0fcf36c91677d7ac8cf07754c7eb659d
Josh <144067649+Josh-moreton@users.noreply.github.com>
2025-11-04 08:47:19 +0000

    remove redundant sections

--- apps/web/src/app/_components/MarketplaceHomeClient.tsx
+++ apps/web/src/app/_components/MarketplaceHomeClient.tsx
@@ -2,7 +2,6 @@
 
 import { Column } from "@buttergolf/ui";
 import { HeroSectionNew } from "./marketplace/HeroSectionNew";
-import { CategoriesSection } from "./marketplace/CategoriesSection";
 import { RecentlyListedSection } from "./marketplace/RecentlyListedSection";
 import { NewsletterSection } from "./marketplace/NewsletterSection";
 import { FooterSection } from "./marketplace/FooterSection";
@@ -11,7 +10,6 @@ export default function MarketplaceHomeClient() {
   return (
     <Column>
       <HeroSectionNew />
-      <CategoriesSection />
       <RecentlyListedSection />
       <NewsletterSection />
       <FooterSection />
