975dff7d2f8b4f2c9d67940069a4300278511c4f
Josh <josh@rwxt.org>
2025-11-24 17:26:35 +0000

    rough splash screen work, needs polish

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -19,6 +19,20 @@ config.resolver.nodeModulesPaths = [
 // #3 - Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
 
+// #3.5 - Configure SVG support with react-native-svg-transformer
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+};
+
 // #4 - Use turborepo to restore the cache when possible
 config.cacheStores = [
   new FileStore({
