50f13faea1c30dd9d3e8cd70521de03e8c40acd1
Josh <josh@rwxt.org>
2026-01-13 15:15:54 +0000

    fix: use @tamagui/babel-plugin instead of deprecated tamagui/babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: [],
-    plugins: ['tamagui/babel', ['module-resolver', { root: ['./'] }]],
+    plugins: ['@tamagui/babel-plugin', ['module-resolver', { root: ['./'] }]],
     // Prevent Next.js (apps/web) from using this root Babel config.
     // Next should use its default SWC pipeline (and @tamagui/next-plugin),
     // while mobile (Expo) uses its own Babel config in apps/mobile.
